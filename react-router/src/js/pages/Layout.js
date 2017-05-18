ae9e13f99223c475df76aae3ba339f3a419ab6b6
Applebaby <queeniezhang0810@gmail.com>
2017-05-18 12:12:58 -0400

    reactjs_router

---
+++ react-router/src/js/pages/Layout.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { Link } from "react-router";
+
+// import Footer from "../components/layout/Footer";
+// import Nav from "../components/layout/Nav";
+
+export default class Layout extends React.Component {
+  
+  navigate(){
+    console.log(this.props);
+    this.props.history.pushState(null,'/');
+  }
+
+  render() {
+    // const { location } = this.props;
+    // const containerStyle = {
+    //   marginTop: "60px"
+    // };
+    // console.log("layout");
+    // return (
+    //   <div>
+
+    //     <Nav location={location} />
+
+    //     <div class="container" style={containerStyle}>
+    //       <div class="row">
+    //         <div class="col-lg-12">
+    //           <h1>KillerNews.net</h1>
+
+    //           {this.props.children}
+
+    //         </div>
+    //       </div>
+    //       <Footer/>
+    //     </div>
+    //   </div>
+
+    // );
+    const {history} = this.props;
+    console.log(history.isActive("archives"));
+    return(
+      <div>
+        <Link to="" class="btn btn-success"> Featured </Link>
+        <Link to="settings" class="btn btn-warning"> Settings </Link>
+        <Link to="archives/nanda" class="btn btn-info"> Archives </Link>
+        <button onClick={this.navigate.bind(this)}>Featured</button>
+        {this.props.children}
+      </div>
+    );
+ 
+  }
+}
